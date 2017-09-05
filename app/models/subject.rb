503e942ba8def18930538d1b08309395e28cbf1b
ALeilands <the.artis+github@gmail.com>
2017-09-05 12:51:18 +0300

    Updated models

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,11 @@
 class Subject < ApplicationRecord
+
+  has_one :page
+
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => false) }
+  scope :sorted, lambda { order(:position => :asc) }
+  scope :newest_first, lambda { order(:created_at => :desc) }
+  scope :search, lambda { |query| where(["name LIKE ?", "%#{query}%"]) }
+
 end
